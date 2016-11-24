9ad13bdb3b65b997cbc5d4b1a204090b5c26772b
Loreinne <estenzoloreinne@gmail.com>
2016-11-25 02:16:50 +0800

    Create new venue [Added]

--- Code/db/functions.sql
+++ Code/db/functions.sql
@@ -25,3 +25,32 @@ $$
     END;
 $$
     LANGUAGE 'plpgsql';
+
+
+create or replace function newvenue(par_name VARCHAR,par_description TEXT, par_categories VARCHAR, par_location VARCHAR,  par_capacity VARCHAR,  par_pricing VARCHAR) returns TEXT AS
+$$
+   DECLARE
+        loc_name VARCHAR;
+        loc_res TEXT;
+    BEGIN
+      SELECT INTO loc_name par_name FROM Venue WHERE name = par_name;
+        if loc_email isnull THEN
+
+      if par_name = '' or par_description = '' or par_categories = ''  or par_location = '' or par_capacity = '' or par_pricing = '' THEN
+        loc_res = 'Error';
+
+      ELSE
+          INSERT INTO Venue (V_name, V_description, V_categories, V_location, V_capacity, V_)
+                        VALUES (par_name, par_description, par_categories, par_location, par_capacity, par_pricing);
+                        loc_res = 'OK';
+          end if;
+
+        ELSE
+          loc_res = 'Error';
+
+        end if;
+        return loc_res;
+
+    END;
+$$
+    LANGUAGE 'plpgsql';
