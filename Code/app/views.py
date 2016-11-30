9235a573194ca0d1f85d8ec354e694368e75e57d
marygracecabolbol <marygrace.cabolbol@g.msuiit.edu.ph>
2016-12-01 06:56:09 +0800

    updated models.py

---
+++ Code/app/views.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify
+from flask.ext.httpauth import HTTPBasicAuth
+from model import DBconn
+import flask
+import sys
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+def spcall(qry, param, commit=False):
+    try:
+        dbo = DBconn()
+        cursor = dbo.getcursor()
+        cursor.callproc(qry, param)
+        res = cursor.fetchall()
+        if commit:
+            dbo.dbcommit()
+        return res
+    except:
+        res = [("Error: " + str(sys.exc_info()[0]) + " " + str(sys.exc_info()[1]),)]
+    return res
+
+
+@app.after_request
+def add_cors(resp):
+    resp.headers['Access-Control-Allow-Origin'] = flask.request.headers.get('Origin', '*')
+    resp.headers['Access-Control-Allow-Credentials'] = True
+    resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET, PUT, DELETE'
+    resp.headers['Access-Control-Allow-Headers'] = flask.request.headers.get('Access-Control-Request-Headers',
+                                                                             'Authorization')
+    # set low for debugging
+
+    if app.debug:
+        resp.headers["Access-Control-Max-Age"] = '1'
+    return resp
+
+if __name__ == '__main__':
+    app.run(debug=True)
