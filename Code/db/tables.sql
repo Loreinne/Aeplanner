854a298b88f2b010a4f0992d7cdd5f0fa2e5f9f5
Loreinne <estenzoloreinne@gmail.com>
2016-12-06 15:59:53 +0800

    [Updated] tables

--- Code/db/tables.sql
+++ Code/db/tables.sql
@@ -1,32 +1,39 @@
 CREATE TABLE App_user (
 	user_id SERIAL PRIMARY KEY,
 	email_address VARCHAR(50),
-	first_name VARCHAR(50),
-	last_name VARCHAR (50),
-	password VARCHAR (50)
+	fname VARCHAR(50),
+	lname VARCHAR (50),
+	password VARCHAR (50),
+	birthdate date,
+	age VARCHAR(5),
+	is_active BOOLEAN DEFAULT TRUE,
  );
 
 
 CREATE TABLE Venue (
-	V_id SERIAL PRIMARY KEY,
-	V_image VARCHAR(50),
-	V_name VARCHAR (50),
-	V_description TEXT,
-	V_categories VARCHAR (50),
-	V_location VARCHAR (50),
-	V_capacity VARCHAR(50),
-	V_pricing VARCHAR (50)
+	id SERIAL PRIMARY KEY,
+	image VARCHAR(50),
+	name VARCHAR (50),
+	description TEXT,
+	categories VARCHAR (50),
+	location VARCHAR (50),
+	capacity VARCHAR(50),
+	pricing VARCHAR (50),
+	cat_id INT references Categories(id),
+	is_active BOOLEAN
 );
 
 CREATE TABLE Catering_services (
-	c_id SERIAL PRIMARY KEY,
-	c_image VARCHAR(50),
-	c_name VARCHAR (50),
-	c_description TEXT,
-	c_categories VARCHAR (50),
-	c_location VARCHAR (50),
-	c_capacity VARCHAR(50),
-	c_pricing VARCHAR (50)
+	id SERIAL PRIMARY KEY,
+	image VARCHAR(50),
+	name VARCHAR (50),
+	description TEXT,
+	categories VARCHAR (50),
+	location VARCHAR (50),
+	capacity VARCHAR(50),
+	pricing VARCHAR (50),
+	cat_id INT references Categories(id),
+	is_active BOOLEAN
 );
 
 CREATE TABLE Proposal (
@@ -47,22 +54,30 @@ CREATE TABLE Contract (
 
 
 CREATE TABLE Event (
-	e_id serial primary key,
-	e_title VARCHAR(50),
-	e_date date,
-	e_time time
+	id serial primary key,
+	title VARCHAR(50),
+	date_event date,
+	time_event time,
+	con_id INT references Contract(id),
+	no_id INT references Note(id),
+	app_id INT references Appointment(id)
 );
 
 CREATE TABLE Note (
-	n_id  serial primary key,
-	n_title VARCHAR (50),
-	n_note Text
+	id  serial primary key,
+	title VARCHAR (50),
+	note Text
 );
 
 CREATE TABLE Appointment (
-	a_id serial primary key,
-	a_title VARCHAR(50),
-	a_date date,
-	a_time time
+	id serial primary key,
+	title VARCHAR(50),
+	app_date date,
+	app_time time
 
+);
+
+CREATE TABLE Categories (
+	id serial primary key,
+	name VARCHAR(50)
 );
